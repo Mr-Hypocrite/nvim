6993c76e4f07bfad74f5696b1a2fb89956464c28
Dinesh Salunke <dineshsalunke@consultbop.com>
2023-06-19 23:42:30 +0530

    refactor: fallback to poimandres theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,14 +3,8 @@ return {
         "dineshsalunke/poimandres.nvim",
         priority = 1000,
         opts = {},
-    },
-
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        opts = {},
         config = function()
-            vim.cmd.colorscheme("rose-pine")
+            vim.cmd.colorscheme("poimandres")
         end,
     },
 }
