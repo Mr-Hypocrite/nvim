8023cda1806cd050a04029a5bd755f38afbe9f99
Dinesh Salunke <dineshsalunke@consultbop.com>
2023-06-26 23:22:31 +0530

    refactor: update some more opts

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,6 +12,8 @@ vim.g.loaded_perl_provider = 0
 
 local opt = vim.opt
 
+opt.hlsearch = false
+opt.incsearch = true
 opt.autowrite = true -- Enable auto write
 opt.clipboard = "unnamedplus" -- Sync with system clipboard
 opt.completeopt = "menu,menuone,noselect"
@@ -47,12 +49,18 @@ opt.splitright = true -- Put new windows right of current
 opt.tabstop = 4 -- Number of spaces tabs count for
 opt.termguicolors = true -- True color support
 opt.timeoutlen = 300
-opt.undofile = true
 opt.undolevels = 10000
-opt.updatetime = 200 -- Save swap file and trigger CursorHold
+opt.updatetime = 50 -- Save swap file and trigger CursorHold
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
 opt.wrap = false -- Disable line wrap
+
+-- opt.colorcolumn = "80"
+
+opt.swapfile = false
+opt.undofile = true
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 
 if vim.fn.has("nvim-0.9.0") == 1 then
     opt.splitkeep = "screen"
