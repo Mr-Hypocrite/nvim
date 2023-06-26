f10295cae90f43371757cc52ef4e8c53f1618ab8
Dinesh Salunke <dineshsalunke@consultbop.com>
2023-06-26 23:23:36 +0530

    refactor: update the colors for poimandres

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,9 +2,26 @@ return {
     {
         "dineshsalunke/poimandres.nvim",
         priority = 1000,
+        opts = {
+            disable_italics = true,
+        },
+        config = function()
+            require("poimandres").setup({
+                groups = {
+                    border = "blueGray1",
+                },
+            })
+            vim.cmd.colorscheme("poimandres")
+        end,
+    },
+
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1001,
         opts = {},
         config = function()
-            vim.cmd.colorscheme("poimandres")
+            -- vim.cmd.colorscheme("tokyonight-night")
         end,
     },
 }
