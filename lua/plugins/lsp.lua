d2ee5f17b1cb0765987611d2799a73f3d1c11d29
Dinesh Salunke <dineshsalunke@consultbop.com>
2023-07-13 21:11:45 +0530

    refactor: add sources to null ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -103,9 +103,16 @@ return {
             null_ls.setup({
                 debug = true,
                 sources = {
+                    null_ls.builtins.code_actions.gitsigns,
+                    null_ls.builtins.diagnostics.editorconfig_checker,
                     null_ls.builtins.formatting.stylua,
                     null_ls.builtins.formatting.prettierd,
                     null_ls.builtins.formatting.eslint_d,
+                    null_ls.builtins.formatting.fixjson,
+                    null_ls.builtins.formatting.gofumpt,
+                    null_ls.builtins.formatting.goimports,
+                    null_ls.builtins.formatting.goimports_reviser,
+                    null_ls.builtins.completion.luasnip,
                 },
             })
         end,
