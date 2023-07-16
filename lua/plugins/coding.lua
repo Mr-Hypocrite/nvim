97eba20a088caec39d13158e3bcd1f26024e8df0
Dinesh Salunke <dineshsalunke@consultbop.com>
2023-07-16 13:27:28 +0530

    refactor: don't use latest version for some packages

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -27,6 +27,7 @@ return {
 
     {
         "hrsh7th/nvim-cmp",
+        version = false,
         event = "InsertEnter",
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        version = false,
         event = {
             "BufReadPost",
             "BufNewFile",
