aa73855d3a4537b9166a7f6b0eeb5a89f8dbc938
abhisheks <abhisheks@consultbop.com>
2023-09-22 14:10:44 +0530

    refactor: add cursorLine color and cleanup color-scheme setup function

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,25 @@
 return {
-
     {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1003,
         opts = {
             neotree = true,
-        },
-        config = function()
-            require("catppuccin").setup({
+            custom_highlights = function(colors)
+                local u = require("catppuccin.utils.colors")
+                return {
+                    CursorLine = {
+                    bg = u.lighten(colors.mantle, 0.90, colors.surface0),
+                },
+                    Visual = {
+                    bg = u.lighten(colors.mantle, 0.10, colors.overlay1),
+                },
+            }
+            end,
                 transparent_background = true
-            })
+        },
+        config = function(_,opts)
+            require("catppuccin").setup(opts)
             vim.cmd.colorscheme("catppuccin-mocha")
         end,
     },
