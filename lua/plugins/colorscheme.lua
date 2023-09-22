0154ad4a9e520045976cb2c41f0d22b2f9404321
abhisheks <abhisheks@consultbop.com>
2023-09-22 12:19:23 +0530

    chore: add transparent bg in catppucin nvim theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,6 +8,9 @@ return {
             neotree = true,
         },
         config = function()
+            require("catppuccin").setup({
+                transparent_background = true
+            })
             vim.cmd.colorscheme("catppuccin-mocha")
         end,
     },
