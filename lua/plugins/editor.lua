3aaf0065649d48ad972ca8b52cef683caf92a9d1
Abhishek <abhishek@Jatins-MacBook-Air.local>
2023-11-10 21:24:40 +0530

    refactor: update transparent colorscheme

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -6,8 +6,22 @@ return {
         priority = 1000,
         opts = {
             neotree = true,
+            custom_highlights = function(colors)
+                local u = require("catppuccin.utils.colors")
+                return {
+                    CursorLine = {
+                    bg = u.lighten(colors.mantle, 0.90, colors.surface0),
+                },
+                    Visual = {
+                    fg = colors.text,
+                    bg = u.lighten(colors.mantle, 0.10, colors.overlay1),
+                },
+            }
+            end,
+                transparent_background = true
         },
-        config = function()
+        config = function(_,opts)
+            require("catppuccin").setup(opts)
             vim.cmd.colorscheme("catppuccin-mocha")
         end,
     },
