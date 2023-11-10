b2f5e1a15c2df6c59c01334ecf31c3d3783098a4
Abhishek <abhishek@Jatins-MacBook-Air.local>
2023-11-10 23:44:37 +0530

    refactor: close neo tree if last window

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -35,6 +35,7 @@ return {
             "MunifTanjim/nui.nvim",
         },
         opts = {
+            close_if_last_window = true,
             filesystem = {
                 follow_current_file = {
                     enabled = true,
