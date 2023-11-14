9b9f5ddc89a6fefe2655efe48dcc07cebf6c8418
Dinesh Salunke <dineshsalunke@consultbop.com>
2023-11-14 23:32:06 +0530

    refactor: add ufo for code folding

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -274,4 +274,30 @@ return {
             })
         end,
     },
+
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = {
+            "kevinhwang91/promise-async",
+        },
+        opt = {},
+        config = function()
+            local ufo = require("ufo")
+            vim.o.foldcolumn = "1"
+            vim.o.foldlevel = 99
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+
+            vim.keymap.set("n", "zR", ufo.openAllFolds)
+            vim.keymap.set("n", "zM", ufo.closeAllFolds)
+            ufo.setup({
+                provider_selector = function()
+                    return {
+                        "treesitter",
+                        "indent",
+                    }
+                end,
+            })
+        end,
+    },
 }
