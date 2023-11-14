08afa192748d4d777be6182a4d4b9a6c7ad34255
Dinesh Salunke <dineshsalunke@consultbop.com>
2023-11-14 23:43:46 +0530

    refactor: add indent-blank-line plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -303,4 +303,32 @@ return {
             })
         end,
     },
+
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = { "BufReadPost", "BufNewFile" },
+        main = "ibl",
+        opts = {
+            indent = {
+                char = "┊",
+            },
+            exclude = {
+                filetypes = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm",
+                    "lazyterm",
+                },
+            },
+            scope = {
+                enabled = false,
+            },
+        },
+    },
 }
